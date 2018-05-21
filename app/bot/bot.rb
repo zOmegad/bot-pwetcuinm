ca480856202a7fbac454c85e04e3f36450302891
EC2 Default User <ec2-user@ip-172-31-13-205.us-east-2.compute.internal>
2018-05-21 08:01:42 +0000

    entacces token changedd

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subsribe(access_token: ENV["ACCES_TOKEN"])
+Facebook::Messenger::Subscriptions.subsribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
     message.reply(texte: "Hello, human!")
