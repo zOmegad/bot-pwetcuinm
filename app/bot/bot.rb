6c58220d9831f8682118260fa1bc4177c217f126
zOmegad <39480336+zOmegad@users.noreply.github.com>
2018-10-21 18:56:19 +0200

    ENV

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: access_token)
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 Bot.on :message do |message|
 
